008eed079eba315690e1fac4ac18215967623d06
Corey <coreymorris@gmx.com>
2020-11-15 17:38:17 -0500

    Further DB testing

--- DB/DBHandler.js
+++ DB/DBHandler.js
@@ -1,11 +1,60 @@
-$(document).ready(function(){
-    $('#DBretrieve').click(function(){
-        console.log('retrieve')
-        let data =  {'connect': true};
-        $.post('./DB/connect.php', data, function (response) {
-            // Response div goes here.
-            let returnedData = JSON.parse(response);
-            alert("action performed successfully" + returnedData[1]);
-        });
-    });
+/*!
+ * TruckLoadCreator (https://github.com/morrisapps/TruckLoadCreator)
+ * Copyright 2020 (c) Corey Morris
+ * Licensed under MIT (https://github.com/morrisapps/TruckLoadCreator/blob/master/LICENSE.md)
+ */
+
+//Creates retrieve dialog
+$(function() {
+    $("#retrieveDialog").dialog({
+        autoOpen: false,
+        show: 'fold',
+        hide: "blind",
+        width: 300,
+        height: 220,
+        buttons : {
+            "Retrieve": function () {
+                console.log(_returnedData);
+                console.log($("#retrieveTruck").val());
+                console.log($("#retrieveTruck")[0].selectedIndex);
+            },
+            "Cancel": function () {
+                $("#retrieveDialog").dialog ("close");
+            }
+        }
+    }).prev().find(".ui-dialog-titlebar-close").hide ();
+    $( "#retrieveTruck" ).selectmenu();
 });
+
+function DBConnect() {
+    $.post('./DB/connect.php', null, function (response) {
+
+        //Returned message
+        _returnedData = JSON.parse(response);
+        if (_returnedData != null){
+            for (let i = 0; i < _returnedData.length; i++){
+                    $('#retrieveTruck').append($('<option>', {
+                        value: _returnedData[i][1],
+                        text: _returnedData[i][1]
+                    })).selectmenu("refresh");
+            }
+        }
+    });
+
+
+
+
+    $(function() {
+        $( "#retrieveDialog" ).dialog( "open" );
+    });
+}
+
+function DBRetrieve(truckID){
+    let data = {'truckID': truckID};
+    $.post('./DB/connect.php', data, function (response) {
+
+        //Returned message
+        let returnedData = JSON.parse(response);
+        alert("action performed successfully" + returnedData);
+    });
+}
--- DB/connect.php
+++ DB/connect.php
@@ -1,17 +1,59 @@
 <?php
-$connectionOptions = array(
+/*
+* TruckLoadCreator (https://github.com/morrisapps/TruckLoadCreator)
+* Copyright 2020 (c) Corey Morris
+* Licensed under MIT (https://github.com/morrisapps/TruckLoadCreator/blob/master/LICENSE.md)
+*/
+
+$return = array();
+$truckID = $_POST['truckID'];
+
+$connectionServer = 'localHost';
+$connectionLogin = array(
     "Database" => "SampleDB",
     "Uid" => "sa",
     "PWD" => "test"
 );
-$return = array();
+$connectionQuery= "SELECT Id, Name, Location FROM TestSchema.Employees;";
+
+
+
 //Establishes the connection
-$conn = sqlsrv_connect("localHost", $connectionOptions);
-array_push($return, 'connecting');
+$conn = sqlsrv_connect($connectionServer, $connectionLogin);
+//array_push($return, $conn);
 if ($conn) {
-    array_push($return, 'connected!');
-} else {
-    array_push($return, 'Not connected');
+    //Read Query
+    $returnedData = sqlsrv_query($conn, $connectionQuery);
+    if ($returnedData == FALSE){
+        die(FormatErrors(sqlsrv_errors()));
+        array_push($return,($row[null] . " " . $row[null] . " " . $row[null] . PHP_EOL));
+    } else {
+        while ($row = sqlsrv_fetch_array($returnedData, SQLSRV_FETCH_ASSOC)) {
+            $truck = array (
+                $row['Id'],
+                $row['Name'],
+                $row['Location']
+            );
+            array_push($return,$truck);
+        }
+    }
+    //Closes query
+    sqlsrv_free_stmt($returnedData);
 }
 echo json_encode($return);
+
+
+
+function FormatErrors( $errors )
+{
+    /* Display errors. */
+    echo json_encode("Error information: ");
+
+    foreach ( $errors as $error )
+    {
+        echo json_encode("SQLSTATE: ".$error['SQLSTATE']."");
+        echo json_encode("Code: ".$error['code']."");
+        echo json_encode("Message: ".$error['message']."");
+    }
+}
 ?>
