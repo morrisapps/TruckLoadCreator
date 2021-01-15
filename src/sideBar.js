1ec9e5f89430f1dc0400d89da317a7d024385af7
Corey <coreymorris@gmx.com>
2021-01-15 15:32:01 -0500

    Sort now sorts by customer and then by width of each unit

--- src/sideBar.js
+++ src/sideBar.js
@@ -797,7 +797,7 @@ function getTagUnit(tag) {
  */
 function sortUnit() {
     units.sort(function (a, b) {
-        return a.drop - b.drop
+        return a.drop - b.drop || a.width - b.width
     });
     units.reverse();
     listUnits();
